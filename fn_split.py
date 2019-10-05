19656db66b69368ed3f1715dc5206bd54f5e98c2
DylanChi <dylanchi@cw.com.tw>
2019-10-05 16:30:43 +0800

    functionize

--- fn_split.py
+++ fn_split.py
@@ -11,20 +11,19 @@ import json
 from sklearn.utils import shuffle
 import sys
 
-def split():
-    ROOT_PATH = os.path.dirname(os.getcwd())
-    SRC_FOLDER = f'{ROOT_PATH}/fma_preprocessing'
-
-    with open(f'{ROOT_PATH}/dylanchi/category_label_mapping.json', 'r') as f:
+def split(srcPath):
+    mappingFile = '/home/jovyan/at082-group23/dylanchi/category_label_mapping.json'
+    with open(mappingFile, 'r') as f:
         categoryMapping = json.load(f)
     iptPath = []
     category = []
     reverseCategoryMapping = {v: k for k, v in categoryMapping.items()}
-    for dirpath, _, filenames in os.walk(SRC_FOLDER):
+    for dirpath, _, filenames in os.walk(srcPath):
         for f in filenames:
-            cat = dirpath.split('/')[4]
+            dirAry = dirpath.split('/')
+            cat = dirAry[4]
             category.append(reverseCategoryMapping[cat])
-            iptPath.append(f'../{dirpath[2:]}/{f}')
+            iptPath.append(f'../{dirAry[2]}/{dirAry[3]}/{dirAry[4]}/{f}')
     dfNew = pd.concat([pd.Series(category), pd.Series(iptPath)], axis=1)
     dfNew.columns = ['category', 'path']
     dfAll = shuffle(dfNew)
