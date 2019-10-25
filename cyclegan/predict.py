fa5927b950b3fb75feae231d9bfdb42f278dcb8a
Jimmy <jimmy60504@gmail.com>
2019-10-25 22:22:41 +0800

    select epoch

--- cyclegan/predict.py
+++ cyclegan/predict.py
@@ -28,6 +28,7 @@ def predict(model, input_filename, output_filename):
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument('-m', '--model', required=True)
+    ap.add_argument('-e', '--epoch', required=False)
     args = ap.parse_args()
 
     MODEL_PATH = os.path.join(MODEL_ROOT_PATH, args.model)
@@ -44,16 +45,24 @@ if __name__ == "__main__":
         discriminator_y_optimizer=discriminator_y_optimizer)
 
     ckpt_manager = tf.train.CheckpointManager(ckpt, MODEL_PATH, max_to_keep=100)
-    ckpt.restore(ckpt_manager.latest_checkpoint)
     last_epoch = len(ckpt_manager.checkpoints)
-    print('Latest checkpoint epoch {} restored!!'.format(last_epoch))
 
-    input_file = [['/home/gtzan/data/gan/wav/sounds/cello/cello-600.wav', ckpt.generator_g],
-                  ['/home/gtzan/data/gan/wav/sounds/sax/sax-600.wav', ckpt.generator_f]]
+    if args.epoch:
+        epoch = args.epoch
+        print('Checkpoint epoch {} restored!!'.format(last_epoch))
+    else:
+        epoch = last_epoch
+        print('Latest checkpoint epoch {} restored!!'.format(last_epoch))
+    epoch -= 1
+    ckpt.restore(ckpt_manager.checkpoints[epoch])
+
+
+    input_file = [['/home/gtzan/data/gan/wav/sounds/piano1/piano1-099.wav', ckpt.generator_g],
+                  ['/home/gtzan/data/gan/wav/sounds/guitar1/guitar1-199.wav', ckpt.generator_f]]
 
     for wav, model in input_file:
         output_file = os.path.join(WAV_PATH,
-                                   os.path.basename(ckpt_manager.latest_checkpoint)
+                                   os.path.basename(ckpt_manager.checkpoints[epoch])
                                    + "-" + os.path.basename(wav))
 
         predict(model, wav, output_file)
