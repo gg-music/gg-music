8a30721bf5ea7a530718900d15d3347e1fdedd23
DylanChi <dylanchi@cw.com.tw>
2019-10-26 14:32:09 +0800

    refactor preprocessing related file

--- cyclegan/preprocessing.py
+++ cyclegan/preprocessing.py
@@ -1,11 +1,41 @@
 import os
+import argparse
+from functools import partial
+from .helpers.parallel import batch_preprocessing, preprocessing
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from .settings import MUSIC_SRC_PATH
-from .helpers.utils import get_file_list, parallel_preprocessing
+from .settings import MUSIC_SRC_PATH, MUSIC_ROOT_PATH
+from .helpers.utils import get_file_list
 from .helpers.signal import to_stft
 
-file_list = get_file_list(MUSIC_SRC_PATH['wav'])
-parallel_preprocessing(file_list,
-                       MUSIC_SRC_PATH['tfrecord'],
-                       spec_format=to_stft,
-                       batch_size=10)
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-s',
+                    '--src_path',
+                    required=True,
+                    help='file src folder root path',
+                    type=str)
+    ap.add_argument('--batch_size',
+                    required=False,
+                    default=10,
+                    help='files per batch',
+                    type=int)
+    ap.add_argument('-tf',
+                    '--tfrecord',
+                    required=False,
+                    default=False,
+                    action='store_true',
+                    help='convert to tfrecord')
+
+    args = ap.parse_args()
+
+    if not os.path.isdir(args.src_path):
+        raise NotADirectoryError('Invalid src path')
+
+    file_list = get_file_list(args.src_path)
+
+    par = partial(batch_preprocessing,
+                  output_dir=MUSIC_ROOT_PATH,
+                  spec_format=to_stft,
+                  to_tfrecord=args.tfrecord)
+
+    preprocessing(file_list, par, args.batch_size)
