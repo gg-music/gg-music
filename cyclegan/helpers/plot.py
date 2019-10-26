fc2d52edc700a0551a91bc880f714f99afc76576
Jimmy <jimmy60504@gmail.com>
2019-10-26 16:00:35 +0800

    sliding window

--- cyclegan/helpers/plot.py
+++ cyclegan/helpers/plot.py
@@ -13,6 +13,7 @@ def plot_epoch_loss(hist, save_dir, n_steps, n_epoch):
         plt.title(f'{model_type} Loss-{n_steps:04} Epoch-{n_epoch:02}')
         plt.xlabel('Steps')
         plt.ylabel('Loss')
+        plt.xlim(left=max(0, len(npy) - 200), right=max(200, len(npy)))
         plt.legend()
 
         make_dirs(f'{save_dir}/{model_type}_loss')
@@ -28,17 +29,12 @@ def plot_heat_map(img, title, save_dir=None):
     img = (img + 1) / 2
 
     fig, ax = plt.subplots(figsize=(4, 4), dpi=100)
-    ax = sns.heatmap(img,
-                     vmin=0,
-                     vmax=1,
-                     ax=ax,
-                     cbar_kws={'orientation': 'horizontal'})
+    ax = sns.heatmap(img, vmin=0, vmax=1, ax=ax, cbar=False)
     ax.set_title(title)
     ax.invert_yaxis()
 
-    make_dirs(save_dir)
-
     if save_dir:
+        make_dirs(save_dir)
         output = os.path.join(save_dir, title + '.png')
         plt.savefig(output, format='png', dpi=100)
         plt.close()
