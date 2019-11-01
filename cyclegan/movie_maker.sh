8fea509c38bbc0dbc65b7bfc56085c438ec5af68
Jimmy <jimmy60504@gmail.com>
2019-11-01 23:57:18 +0800

    flip x y

--- cyclegan/movie_maker.sh
+++ cyclegan/movie_maker.sh
@@ -2,12 +2,12 @@ model=$1
 MODEL_ROOT=/home/gtzan/ssd/models/$model
 python -m cyclegan.plot_history -m $model
 mkdir $MODEL_ROOT/mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/fake_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/fake_x.mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/fake_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/fake_y.mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_fake_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_fake_x.mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_fake_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_fake_y.mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_real_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_real_x.mp4
-ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_real_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_real_y.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/fake_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/fake_x.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/fake_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/fake_y.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_fake_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_fake_x.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_fake_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_fake_y.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_real_x/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_real_x.mp4
+ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/disc_real_y/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/disc_real_y.mp4
 ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/Generator_loss/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/Generator_loss.mp4
 ffmpeg -y -framerate 24 -pattern_type glob -i $MODEL_ROOT'/images/Discriminator_loss/*.png' -c:v libx264 -pix_fmt yuv420p $MODEL_ROOT/mp4/Discriminator_loss.mp4
 ffmpeg -y -i $MODEL_ROOT/mp4/fake_x.mp4 -i $MODEL_ROOT/mp4/fake_y.mp4 \
