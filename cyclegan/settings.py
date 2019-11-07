43bcd28d5c047bf0d23cbb46a56b3b942e4b91fb
Jimmy <jimmy60504@gmail.com>
2019-11-07 10:45:41 +0800

    change steps and epoch

--- cyclegan/settings.py
+++ cyclegan/settings.py
@@ -1,11 +1,11 @@
 RAWSET_PATH = '/home/gtzan/data/gan_preprocessing/tfrecords'
 RAWDATA_ROOT_PATH = '/home/gtzan/data'
 MODEL_ROOT_PATH = '/home/gtzan/ssd/models'
-EPOCHS = 20
+EPOCHS = 5
 DEFAULT_SAMPLING_RATE = 22050
 DROPOUT_RATE = 0.5
 BN_AXIS = 3
-STEPS = 3000
+STEPS = 10000
 WAVS_TO_PREDICT_ROOT_PATH = RAWDATA_ROOT_PATH + '/gan_preprocessing/wav'
 # trim_cqt=1.484
 # trim_stft=5.9
